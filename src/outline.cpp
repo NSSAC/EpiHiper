4b95fdd0705743edb2df448e7772c48c82fa701b
Jiangzhuo Chen <chenj@virginia.edu>
2019-05-22 14:06:41 -0400

    initial outline of EpiHiper codes (classes and methods)

---
+++ src/outline.cpp
@@ -0,0 +1,86 @@
+void Simulation::run() {
+  // process initialization
+  for (std::vector<Intervention>::iterator init = initialization.begin();
+       init != initialization.end(); ++init) {
+    std::vector<Node> candidates = init->computeCandidates(network,population);
+    for (std::vector<Node>::iterator node = candidates.begin();
+	 node != candidates.end(); ++node) {
+      // schedule action for node to actionQueue
+    }
+  }
+
+  // loop
+  int tick = startTick;
+  while (tick <= endTick) {
+    std::vector<Action> scheduledActions = actionQueue->getActions(tick);
+    actionQueue->clearActions(tick);
+    for (std::vector<Action>::iterator it = scheduledActions.begin();
+	 it != scheduledActions.end(); ++it) {
+      it->checkConditions();
+      if (it->conditionsTrue()) {
+	execute(*it);
+      }
+    }
+    // broadcast and receive all changes
+    compEnv->exchange(localChanges);
+    // broadcast and receive non-local actions ???
+
+    // write changes to output
+    // one PE writes all changes to file
+    // or each PE writes local changes to DB
+
+    tick++;
+
+    // process infections
+    for (std::vector<Node>::iterator node = population.begin();
+	 node != population.end(); ++node) {
+      if (node->isLocal()) {
+	nextState = node->computeInfection(disease,network,population);
+	if (nextState != NULL) {
+	  // schedule action to actionQueue
+	  // condition = node->getCurrentState(); priority = 1
+	}
+      }
+    }
+
+    processInterventions();
+  }
+}
+
+void Simulation::processInterventions() {
+  for (std::vector<Intervention>::iterator intv = interventions.begin();
+       intv != interventions.end(); ++intv) {
+    std::vector<Node> candidates = intv->computeCandidates(network,population);
+    for (std::vector<Node>::iterator node = candidates.begin();
+	 node != candidates.end(); ++node) {
+      // schedule action for node to actionQueue
+    }
+  }
+}
+
+Simulation::execute(Action action) {
+  // execute assignment
+  // scaling factors, traits, (de)activate edge, health state
+
+  if (action.isStateTransition()) {
+    // compute next state transition
+    Node node = action.getTarget();
+    Transition transition = disease.getTransition(node.currentState);
+    health_t nextState = transition.nextState;
+    int delay = transition.delay; // ticks to transition
+    // schedule (node,nextState,delay,condition=currentState,priority=1)
+    // to actionQueue
+  }
+  // cascading events
+  processInterventions();
+}
+
+health_t Node::computeInfection
+(DiseaseModel *disease, Network *network, std::vector<Node> *population) {
+  std::vector<pid_t> infectors = network->getNeighbors(id);
+  health_t nextState = NULL;
+  // get node states of infectors from population; compute total prob.;
+  // if infection occurs compute next state
+  return nextState;
+}
+
---
+++ src/outline.h
@@ -0,0 +1,147 @@
+class SimConfig {
+private:
+  bool validated;
+  // required parameters
+  std::string diseaseModelFile;
+  std::string networkFile;
+  std::string interventionFile;
+  int startTick;
+  int endTick;
+  int numReplicates; // number of replicates to run
+
+  // optional
+  std::string outputFile;
+  std::string logFile;
+  std::string dbServiceUrl;
+  std::string rngFile; // random number generating seeds
+
+public:
+  SimConfig(std::string configFile);
+  ~SimConfig();
+
+  // minimal set of parameters are specified and files are accessible
+  validate();
+};
+
+class PersonTraitDB {
+private:
+  std::string url; // ip:port
+  bool connected;
+
+public:
+  PersonTraitDB();
+  virtual ~PersonTraitDB();
+
+  // establish connection to DB service
+  connect();
+  // terminate connection to DB service
+  disconnect();
+
+  // send query and get results
+  std::string query(std::string statement);
+
+  // need a few parsing methods, e.g.
+  // pid_t parse(std::string queryResults);
+
+  // write data to DB; data in JSON, with metadata (query statement)
+  bool write(std::string data);
+};
+
+class Simulation {
+private:
+  int startTick;
+  int endTick;
+  std::vector<Node> population; // the set of all nodes
+  std::vector<Intervention> initialization;
+  std::vector<Intervention> interventions;
+  DiseaseModel *disease;
+  Network *network;
+  ActionQueue *actionQueue;
+  ComputingEnvironment *compEnv; // MPI context
+  PersonTraitDB *db;
+  Output *output;
+  Log *log;
+
+public:
+  Simulation(SimConfig cfg);
+  ~Simulation();
+  void run();  
+};
+
+class Network {
+private:
+  // TBD: data structure to store node ids and incoming edges
+public:
+  std::vector<pid_t> getNeighbors(pid_t id);
+  bool isLocal(pid_t id);
+};
+
+class Node {
+private:
+  pid_t id;
+  health_t currentState;
+  bool local;
+public:
+  Node(pid_t id);
+  ~Node();
+  pid_t getId();
+  health_t getCurrentState();
+  bool isLocal();
+  health_t computeInfection(DiseaseModel *disease, Network *network,
+			    Population *pop);
+};
+
+class DiseaseModel {
+private:
+  // diagram
+public:
+  DiseaseModel(std::string modelJson);
+  ~DiseaseModel();
+  Transition getTransition(health_t currentState);
+};
+
+class Intervention {
+private:
+  // filters, actions, etc.
+public:
+  Intervention(std::string interventionJson);
+  ~Intervention();
+  std::vector<Node> computeCandidates(Network *network,
+				      std::vector<Node> population);
+};
+
+class Action {
+private:
+  bool conditionsTrue;
+  std::vector<ActionCondition> conditions;
+public:
+  Action();
+  ~Action();
+  checkConditions();
+  conditionsTrue();
+};
+
+class ActionQueue {
+private:
+  std::vector<Action> actions;
+public:
+  ActionQueue();
+  ~ActionQueue();
+  void schedule(Action action);
+  // get actions scheduled for tick
+  std::vector<Action> getActions(int tick);
+  // removed actions scheduled for tick
+  void clearActions(int tick);
+};
+
+class ComputingEnvironment {
+private:
+  // PE rank, master PE for global variables, etc.
+public:
+  void exchange(changes);
+};
+
+struct Transition {
+  health_t nextState;
+  int delay;
+};
