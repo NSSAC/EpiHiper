14082a5e553240ee71358ffd45d49d4c16e1ab62
Jiangzhuo Chen <chenj@virginia.edu>
2019-05-28 10:59:59 -0400

    type definitions

---
+++ src/Types.h
@@ -0,0 +1,31 @@
+#ifndef EPIHIPER_TYPES_H
+#define EPIHIPER_TYPES_H
+
+#include <cstdint>
+#include <string>
+
+typedef uint64_t pid_t;
+struct health_t {
+  std::string id;
+  std::string label;
+  float susceptibility;
+  float infectivity;
+};
+struct infection_t {
+  std::string id;
+  std::string label;
+  health_t entryState;
+  health_t exitState;
+  health_t contactState;
+  float transmissibility;
+};
+struct transition_t {
+  std::string id;
+  std::string label;
+  health_t entryState;
+  health_t exitState;
+  float prob;
+  // Distribution dwellTime; // TODO: class Distribution
+};
+
+#endif
