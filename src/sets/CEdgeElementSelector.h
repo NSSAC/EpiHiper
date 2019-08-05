cd310c300f781170b767b58338ca7914eac30d3d
Stefan Hoops <shoops@copasi.org>
2019-08-05 14:26:12 -0400

    Added Copyright and License

--- src/sets/CEdgeElementSelector.h
+++ src/sets/CEdgeElementSelector.h
@@ -1,3 +1,15 @@
+// BEGIN: Copyright 
+// Copyright (C) 2019 Rector and Visitors of the University of Virginia 
+// All rights reserved 
+// END: Copyright 
+
+// BEGIN: License 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//   http://www.apache.org/licenses/LICENSE-2.0 
+// END: License 
+
 #ifndef SRC_SETS_CEDGEELEMENTSELECTOR_H_
 #define SRC_SETS_CEDGEELEMENTSELECTOR_H_
 
