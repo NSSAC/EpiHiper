5a1297c3cb884c9756b4628565d9b2fbc203332d
Stefan Hoops <shoops@copasi.org>
2019-08-21 16:36:08 -0400

    Fixed loading of DB schema.

--- src/db/CSchema.cpp
+++ src/db/CSchema.cpp
@@ -10,6 +10,7 @@
 //   http://www.apache.org/licenses/LICENSE-2.0 
 // END: License 
 
+#include <fstream>
 #include <jansson.h>
 
 #include "db/CSchema.h"
@@ -27,7 +28,25 @@ void CSchema::load(const std::vector< std::string > & schemaFiles)
 
   for (; it != end; ++it)
     {
-      json_t * pRoot = SimConfig::loadJson(*it, JSON_DECODE_INT_AS_REAL);
+	  std::ifstream is(it->c_str());
+
+	  if (is.fail())
+	    {
+	      std::cerr << "Network file: '" << *it << "' cannot be opened." << std::endl;
+	      return;
+	    }
+
+	  std::string Line;
+	  std::getline(is, Line);
+
+	  json_error_t error;
+
+	  json_t * pRoot = json_loads(Line.c_str(), 0, &error);
+
+	  if (pRoot == NULL)
+	    {
+	      std::cerr << "Network file: '" << *it << "' error on line " << error.line << ": " << error.text << std::endl;;
+	    }
 
       if (pRoot != NULL)
         {
