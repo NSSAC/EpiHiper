48a11ba1e518ad0c20bddd973f4e3748d1c164e3
Stefan Hoops <shoops@copasi.org>
2020-03-06 13:47:04 -0500

    Added 60 second timeout to connection.

--- src/db/CConnection.cpp
+++ src/db/CConnection.cpp
@@ -1,14 +1,14 @@
-// BEGIN: Copyright
-// Copyright (C) 2019 - 2020 Rector and Visitors of the University of Virginia
-// All rights reserved
-// END: Copyright
+// BEGIN: Copyright 
+// Copyright (C) 2019 - 2020 Rector and Visitors of the University of Virginia 
+// All rights reserved 
+// END: Copyright 
 
-// BEGIN: License
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//   http://www.apache.org/licenses/LICENSE-2.0
-// END: License
+// BEGIN: License 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//   http://www.apache.org/licenses/LICENSE-2.0 
+// END: License 
 
 #include <iostream>
 #include "db/CConnection.h"
@@ -37,6 +37,8 @@ void CConnection::init()
     }
 
   URI += dbConnection.host + "/" + dbConnection.name;
+
+  URI += "?connect_timeout=60";
 
   try
     {
