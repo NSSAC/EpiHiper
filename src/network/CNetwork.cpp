7b5e7b8e447de0bb2b00ca298b6ca2adf208ff5d
Stefan Hoops <shoops@copasi.org>
2024-04-03 18:42:45 -0400

    Fixed initialization of first node.

--- src/network/CNetwork.cpp
+++ src/network/CNetwork.cpp
@@ -1,7 +1,7 @@
 // BEGIN: Copyright 
 // MIT License 
 //  
-// Copyright (C) 2019 - 2023 Rector and Visitors of the University of Virginia 
+// Copyright (C) 2019 - 2024 Rector and Visitors of the University of Virginia 
 //  
 // Permission is hereby granted, free of charge, to any person obtaining a copy 
 // of this software and associated documentation files (the "Software"), to deal 
@@ -1010,12 +1010,11 @@ void CNetwork::load()
     CNode * pNode = Active.mLocalNodes;
     CNode * pNodeEnd = pNode + Active.mLocalNodesSize;
 
-    CNode DummyNode = DefaultNode;
+    *pNode = DefaultNode;
 
     // Handle a partition without any nodes
     if (Active.mLocalNodesSize == 0)
       {
-        pNode = &DummyNode;
         pNodeEnd = pNode;
       }
 
