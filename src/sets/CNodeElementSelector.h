48833e4ebccbc62d296aa2f6f79982ffd1d4eb39
Stefan Hoops <shoops@copasi.org>
2024-07-31 17:22:59 -0400

    Added complement withEdgeNotIn to withEdgeIn

--- src/sets/CNodeElementSelector.h
+++ src/sets/CNodeElementSelector.h
@@ -1,7 +1,7 @@
 // BEGIN: Copyright 
 // MIT License 
 //  
-// Copyright (C) 2019 - 2023 Rector and Visitors of the University of Virginia 
+// Copyright (C) 2019 - 2024 Rector and Visitors of the University of Virginia 
 //  
 // Permission is hereby granted, free of charge, to any person obtaining a copy 
 // of this software and associated documentation files (the "Software"), to deal 
@@ -68,7 +68,8 @@ private:
   bool propertySelection();
   bool propertyIn();
   bool propertyNotIn();
-  bool withIncomingEdge();
+  bool withIncomingEdgeIn();
+  bool withIncomingEdgeNotIn();
   bool dbAll();
   bool dbSelection();
   bool dbIn();
